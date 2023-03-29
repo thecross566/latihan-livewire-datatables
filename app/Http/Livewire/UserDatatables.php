192d445f6048aa29c90a511f926f39f1a3c88c43
thecross566 <arulprosg@gmail.com>
2023-03-29 11:23:10 +0700

    First Commit

---
+++ app/Http/Livewire/UserDatatables.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+use Mediconesystems\LivewireDatatables\DateColumn;
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+
+class UserDatatables extends LivewireDatatable
+{
+    public $model = User::class;
+
+    public function columns()
+    {
+        return [
+            NumberColumn::name('id')->label('ID')->sortBy('id'),
+            Column::name('name')->label('Name'),
+            Column::name('email')->label('Email'),
+            DateColumn::name('created_at')->label('Creation Date')
+        ];
+    }
+}
